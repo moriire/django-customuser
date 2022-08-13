725d439569bca1239888363db83f08e4da1a013a
moriire <ibraheemabdulsalam@gmail.com>
2022-08-13 09:46:42 +0100

    latest

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'django_customuser',
+    packages = ['customuser'],
+    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'IBM Abdulsalam',
+    author_email = 'ibmabdulsalam@gmail.com',
+    url = 'github package source url',
+    download_url = "https://github.com/moriire/django_customuser/archive/refs/tags/django_customuser_1.0.0.tar.gz", 
+    keywords = ['django_customuser', 'django_customuser'],
+    classifiers = [],
+)
